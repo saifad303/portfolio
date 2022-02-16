5dbf6c00148c8717f1792208c1ceb1c8038c4419
Saif Ahmed <saifad303@gmail.com>
2022-02-16 23:36:24 +0600

    link updated

--- components/Main.js
+++ components/Main.js
@@ -26,7 +26,7 @@ function Main() {
         <Gmail />
         <Facebook />
       </div>
-      <Link href="https://drive.google.com/file/d/100sjGx5-hAn17zXhijR9y9U7UZU3toNZ/view?usp=sharing">
+      <Link href="https://drive.google.com/file/d/1jRwOt7Bs9vBtjRE229ijsXZF3rU-xPpU/view?usp=sharing">
         <button className="text-white bg-[#CC4B2C] font-medium px-7 py-4 rounded-md mt-10 hover:bg-[#DF502E]">
           Download Resume
         </button>
